791de3dc332828fc13de5fe31d2adf1c6fb2e2b7
wierzba100 <filipwierzbinka@gmail.com>
2024-06-13 23:29:48 +0200

    PC APP update

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy as np
 from bleak import BleakClient, BleakScanner
 from bleak.backends.characteristic import BleakGATTCharacteristic
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import threading
 
 
 class HorizontalAndVerticalDegsPlusFlag:
@@ -13,49 +15,54 @@ class HorizontalAndVerticalDegsPlusFlag:
 
 degs_plus_flag = HorizontalAndVerticalDegsPlusFlag(0, -45, 1)
 
+x_data = []
+y_data = []
+z_data = []
 
-def update_graph(azimuth_angle, elevation_angle, distance):
+
+def update_graph_data(azimuth_angle, elevation_angle, distance):
     azimuth_angle_rad = np.deg2rad(azimuth_angle)
     elevation_angle_rad = np.deg2rad(elevation_angle)
 
     x = distance * np.cos(elevation_angle_rad) * np.cos(azimuth_angle_rad)
     y = distance * np.cos(elevation_angle_rad) * np.sin(azimuth_angle_rad)
     z = distance * np.sin(elevation_angle_rad)
-
+    
     x_data.append(x)
     y_data.append(y)
     z_data.append(z)
-
-    ax.scatter(x_data, y_data, z_data, c='r', marker='o', s=5)
-    plt.draw()
-    plt.pause(0.5)
 
 
 async def notification_handler(characteristic: BleakGATTCharacteristic, data: bytearray):
     decimal_values = [int(byte) for byte in data]
     distance = (decimal_values[0] << 8) + decimal_values[1]
     strength = (decimal_values[2] << 8) + decimal_values[3]
-    print(strength)
-    if(distance > 1200):
+    
+    if distance > 1200:
         distance = 1200
 
-    update_graph(degs_plus_flag.horizontal_deg, distance, degs_plus_flag.vertical_deg)
-    degs_plus_flag.horizontal_deg = degs_plus_flag.horizontal_deg + 1
-    if(degs_plus_flag.horizontal_deg == 180):
+    update_graph_data(degs_plus_flag.horizontal_deg, degs_plus_flag.vertical_deg, distance)
+    degs_plus_flag.horizontal_deg += 1
+    if degs_plus_flag.horizontal_deg == 180:
         degs_plus_flag.horizontal_deg = 0
-        degs_plus_flag.vertical_deg = degs_plus_flag.vertical_deg + 1
-        if(degs_plus_flag.vertical_deg == 45):
+        degs_plus_flag.vertical_deg += 1
+        if degs_plus_flag.vertical_deg == 44:
             degs_plus_flag.flag_to_stop = 0
+
 
 async def client_loop(lidar_scanner: BleakClient):
     async with BleakClient(lidar_scanner) as client:
         print("Connected")
         await client.write_gatt_char("932ee1e0-547a-4ef8-93f7-843ffa16afab", b'\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11', False)
         await client.start_notify("932ee1df-547a-4ef8-93f7-843ffa16afab", notification_handler)
-        while(degs_plus_flag.flag_to_stop):
+        while degs_plus_flag.flag_to_stop:
             await asyncio.sleep(1.0)
         await client.stop_notify("932ee1df-547a-4ef8-93f7-843ffa16afab")
         await client.write_gatt_char("932ee1e0-547a-4ef8-93f7-843ffa16afab", b'\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF', False)
+
+
+def run_client_loop(lidar_scanner):
+    asyncio.run(client_loop(lidar_scanner))
 
 
 if __name__ == '__main__':
@@ -72,11 +79,18 @@ if __name__ == '__main__':
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
 
-    x_data = []
-    y_data = []
-    z_data = []
-
-    if (lidar_scanner == None):
+    if lidar_scanner is None:
         print("Device not found!")
     else:
-        loop.run_until_complete(client_loop(lidar_scanner))
+        client_thread = threading.Thread(target=run_client_loop, args=(lidar_scanner,))
+        client_thread.start()
+        
+        plt.ion()
+        while degs_plus_flag.flag_to_stop:
+            ax.scatter(x_data, y_data, z_data, c='r', marker='o', s=1)
+            plt.draw()
+            plt.pause(0.5)
+        
+        plt.savefig("scan.png")
+        plt.ioff()
+        plt.show()
